2829d2e4b8b2d0ad0fa325ba860d2bee8c86d3b9
Jesus Muñoz G <146115787+Jesus0Munoz@users.noreply.github.com>
2024-02-21 15:30:12 -0500

    Creación de un método Constructor en JAVA

---
+++ Main.java
@@ -0,0 +1,17 @@
+package Constructor;
+
+public class Main {
+
+public static void main(String[] args) {
+    
+    Persona per= new Persona ("Alex", 15);
+
+    //per.inicializar("Juan", 18);
+
+
+    System.out.println("Nombre = " + per.nombre + " | Edad = " + per.edad);
+
+
+}
+
+}
---
+++ Persona.java
@@ -0,0 +1,21 @@
+package Constructor;
+
+public class Persona {
+
+    String nombre;
+    int edad;
+
+//    public void inicializar(String nombre, int edad){
+//
+  //      this.nombre = nombre;
+    //    this.edad = edad;
+
+    public Persona (String nombre, int edad){
+     this.nombre = nombre;
+     this.edad = edad;
+
+    }
+
+    //}
+    
+}
